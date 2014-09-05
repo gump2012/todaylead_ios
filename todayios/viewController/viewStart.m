277dfc109d6de0f84eaf6fa7d6ab53f3419eb81d
gump2012 <yige2002@tom.com>
2014-09-05 21:10:38 +0800

    new picture

--- todayios/viewController/viewStart.m
+++ todayios/viewController/viewStart.m
@@ -33,27 +33,36 @@ static viewStart * shareins = nil;
     shopview.tabBarItem = item;
     UINavigationController *navshop = [[UINavigationController alloc] initWithRootViewController:shopview];
     navshop.tabBarItem.title = @"商城";
+    [navshop.tabBarItem setImage:[UIImage imageNamed:@"icon_bottom_cod_n.png"]];
+    [navshop.tabBarItem setSelectedImage:[UIImage imageNamed:@"icon_bottom_cod_c.png"]];
     
     categoryViewController *categoryview = [[categoryViewController alloc] init];
     categoryview.title = @"分类";
     item = [[UITabBarItem alloc] initWithTitle:@"分类" image:nil tag:2];
     categoryview.tabBarItem = item;
     UINavigationController *navcategory = [[UINavigationController alloc] initWithRootViewController:categoryview];
+    [navcategory.tabBarItem setImage:[UIImage imageNamed:@"icon_bottom_category_n.png"]];
+    [navcategory.tabBarItem setSelectedImage:[UIImage imageNamed:@"icon_bottom_category_c.png"]];
     
     paymentViewController *paymentview = [[paymentViewController alloc] init];
     paymentview.title = @"购物车";
     item = [[UITabBarItem alloc] initWithTitle:@"购物车" image:nil tag:3];
     paymentview.tabBarItem = item;
     UINavigationController *navpayment = [[UINavigationController alloc] initWithRootViewController:paymentview];
+    [navpayment.tabBarItem setImage:[UIImage imageNamed:@"icon_bottom_shopcar_n.png"]];
+    [navpayment.tabBarItem setSelectedImage:[UIImage imageNamed:@"icon_bottom_shopcar_c.png"]];
     
     meViewController *meview = [[meViewController alloc] init];
     meview.title = @"我";
     item = [[UITabBarItem alloc] initWithTitle:@"我" image:nil tag:4];
     meview.tabBarItem = item;
     UINavigationController *navme = [[UINavigationController alloc] initWithRootViewController:meview];
+    [navme.tabBarItem setImage:[UIImage imageNamed:@"icon_bottom_my_n.png"]];
+    [navme.tabBarItem setSelectedImage:[UIImage imageNamed:@"icon_bottom_my_c.png"]];
     
     UITabBarController *tabview = [[UITabBarController alloc] init];
     tabview.viewControllers = [NSArray arrayWithObjects:navshop, navcategory,navpayment,navme,nil];
+    tabview.tabBar.selectedImageTintColor = [UIColor redColor];
     [CP shareInstance].mytabbar = tabview;
     window.rootViewController = tabview;
     [window makeKeyAndVisible];
